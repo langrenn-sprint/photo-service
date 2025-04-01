bfbd9354819025a5aac9416416c42dc62d193da9
Heming Harnaes <heming.harnaes@gmail.com>
2025-04-01 22:16:14 +0200

    Bugfix #53

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,9 +43,9 @@ def http_service(docker_ip: Any, docker_services: Any) -> Any:
     """Ensure that HTTP service is up and responsive."""
     # `port_for` takes a container port and returns the corresponding host port
     port = docker_services.port_for("photo-service", HOST_PORT)
-    url = "http://{}:{}".format(docker_ip, port)
+    url = f"http://{docker_ip}:{port}"
     docker_services.wait_until_responsive(
-        timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
+        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
     )
     return url
 
